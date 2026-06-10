deea0e0e170621ee84db425ac725064383d2360b
sgghomepro-collab <sgg.homepro@gmail.com>
2026-06-10 15:23:13 +0000

    Fix Button asChild React warning

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -42,10 +42,14 @@ const buttonVariants = cva(
 
 function Button({
   className,
-  variant = 'default',
-  size = 'default',
+  variant = "default",
+  size = "default",
+  asChild,
   ...props
-}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+}: ButtonPrimitive.Props &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
   return (
     <ButtonPrimitive
       data-slot="button"
